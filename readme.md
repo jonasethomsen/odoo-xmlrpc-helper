0bdbe2369d76895c9c514b9f619299e9fdab2be9
Jonas <jonasthomsen@gmail.com>
2023-10-05 14:32:39 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,65 @@
+# Odoo Client TypeScript Module
+
+This is a private module that provides a TypeScript client for interacting with the Odoo XML-RPC API.
+
+## Features
+
+- Authentication
+- CRUD Operations
+- Custom API Calls
+- Type Safety with TypeScript
+
+## Requirements
+
+- Node.js
+- Odoo instance with XML-RPC enabled
+
+## Installation
+
+Since this package is hosted privately on GitHub, you can install it using:
+
+\`\`\`bash
+npm install git+https://github.com/your-username/your-repo.git#semver:^1.0.0
+\`\`\`
+
+Replace `your-username` and `your-repo` with the appropriate GitHub username and repository name. Adjust the semver tag as needed.
+
+## Usage
+
+First, make sure you have environment variables or configurations set up for the Odoo instance you're connecting to.
+
+\`\`\`javascript
+import { createOdooClient } from 'odoo-client-ts';
+
+const client = createOdooClient({
+  url: 'http://localhost:8069',
+  db: 'my_database',
+  username: 'admin',
+  password: 'password'
+});
+
+async function exampleUsage() {
+  const uid = await client.login();
+  const partners = await client.search('res.partner', [['is_company', '=', true]]);
+  const partnerData = await client.read('res.partner', partners, ['name', 'country_id', 'comment']);
+  console.log(partnerData);
+}
+
+exampleUsage();
+\`\`\`
+
+## Testing
+
+This module includes unit tests. Run the tests with:
+
+\`\`\`bash
+npm test
+\`\`\`
+
+## Contributing
+
+This is a private package, so contributions are not accepted.
+
+## License
+
+This package is for internal usage only and is not licensed for external distribution.
---
+++ src/index.ts
@@ -0,0 +1,265 @@
+import { XmlRpcClient } from "@foxglove/xmlrpc";
+
+const configUrl = process.env.ODOO_URL;
+const configDB = process.env.ODOO_DB;
+const configUsername = process.env.ODOO_USERNAME;
+const configPassword = process.env.ODOO_PASSWORD;
+
+interface OdooConfig {
+  url: string | undefined;
+  db: string | undefined;
+  username: string | undefined;
+  password: string | undefined;
+}
+
+interface OdooClient {
+  login: () => Promise<number | null>;
+  call: (
+    model: string,
+    method: string,
+    args?: any[],
+    kwargs?: any
+  ) => Promise<any>;
+  search: (
+    model: string,
+    domain: any[],
+    kwargs?: any,
+    context?: object
+  ) => Promise<any>;
+  read: (
+    model: string,
+    ids: any[],
+    fields?: any[],
+    context?: object
+  ) => Promise<any>;
+  get: (model: string, ids: any[], context?: object) => Promise<any>;
+  write: (model: string, ids: any[], values: any) => Promise<any>;
+  create: (model: string, values: any) => Promise<any>;
+  unlink: (model: string, ids: any[]) => Promise<any>;
+  fieldsGet: (model: string, kwargs?: any) => Promise<any>;
+  nameGet: (model: string, ids: any[]) => Promise<any>;
+  nameSearch: (
+    model: string,
+    name: string,
+    operator?: string,
+    kwargs?: any
+  ) => Promise<any>;
+  nameCreate: (model: string, name: string) => Promise<any>;
+  defaultGet: (model: string, fields: any[]) => Promise<any>;
+  onChange: (
+    model: string,
+    ids: any[],
+    method: string,
+    args: any[]
+  ) => Promise<any>;
+  workflowSignal: (model: string, ids: any[], signal: string) => Promise<any>;
+  searchRead: (
+    model: string,
+    domain: any[],
+    fields?: any[],
+    kwargs?: any,
+    context?: object
+  ) => Promise<any>;
+  filterGetResults: any
+}
+
+function parseDomain(input: string | undefined): any {
+  if (input === undefined) {
+    return null;  // Or however you want to handle undefined input
+  }
+
+  let domain = input
+    .replace(/'/g, '"')
+    .replace(/(\\n)/g, "")
+    .replace(/(\\r)/g, "")
+    .replace(/(\\t)/g, "")
+    .replace(/(\\f)/g, "")
+    .replace(/(\\b)/g, "")
+    .replace(/("{)/g, "{")
+    .replace(/(}")/g, "}")
+    .replace(/(\")/g, "\"")
+    .replace(/(\\)/g, "")
+    .replace(/(\/)/g, "/")
+    .replace(/(True)/g, "\"TRUE\"")
+    .replace(/(False)/g, "\"FALSE\"");  // Added this line for False
+
+  try {
+    return JSON.parse(domain);
+  } catch (e) {
+    console.error("Failed to parse domain:", e);
+    return null;  // Or however you want to handle parsing failures
+  }
+}
+
+function createOdooClient(config?: OdooConfig): OdooClient {
+  config = config ?? {
+    url: configUrl,
+    db: configDB,
+    username: configUsername,
+    password: configPassword,
+  };
+
+  const url = config.url;
+  const db = config.db;
+  const username = config.username;
+  const password = config.password;
+
+  const commonClient = new XmlRpcClient(`${url}/xmlrpc/2/common`);
+  const objectClient = new XmlRpcClient(`${url}/xmlrpc/2/object`);
+  let uid: number | null = null;
+
+  async function login() {
+    if (!uid) {
+      // @ts-ignore
+      uid = await commonClient.methodCall("authenticate", [
+        db,
+        username,
+        password,
+        {},
+      ]);
+    }
+    return uid;
+  }
+
+  async function call(
+    model: string,
+    method: string,
+    args: any[] = [],
+    kwargs: any = {}
+  ) {
+    const uid = await login();
+    return await objectClient.methodCall("execute_kw", [
+      db,
+      uid,
+      password,
+      model,
+      method,
+      args,
+      kwargs,
+    ]);
+  }
+
+  async function search(
+    model: string,
+    domain: any[],
+    kwargs: any = {},
+    context: any = {}
+  ) {
+    return await call(model, "search", [domain], { ...kwargs, context });
+  }
+
+  async function read(
+    model: string,
+    ids: any[],
+    fields: any[] = [],
+    context: any = {}
+  ) {
+    return await call(model, "read", [ids, fields], { context });
+  }
+  async function get(model: string, ids: any[], context: any = {}) {
+    return await call(model, "read", [ids], { context });
+  }
+
+  async function write(model: string, ids: any[], values: any) {
+    return await call(model, "write", [ids, values]);
+  }
+
+  async function create(model: string, values: any) {
+    return await call(model, "create", [values]);
+  }
+
+  async function unlink(model: string, ids: any[]) {
+    return await call(model, "unlink", [ids]);
+  }
+
+  async function fieldsGet(model: string, kwargs: any = {}) {
+    return await call(model, "fields_get", [], kwargs);
+  }
+
+  async function nameGet(model: string, ids: any[]) {
+    return await call(model, "name_get", [ids]);
+  }
+
+  async function nameSearch(
+    model: string,
+    name: string,
+    operator = "ilike",
+    kwargs: any = {}
+  ) {
+    return await call(model, "name_search", [name, operator], kwargs);
+  }
+
+  async function nameCreate(model: string, name: string) {
+    return await call(model, "name_create", [name]);
+  }
+
+  async function defaultGet(model: string, fields: any[]) {
+    return await call(model, "default_get", [fields]);
+  }
+
+  async function onChange(
+    model: string,
+    ids: any[],
+    method: string,
+    args: any[]
+  ) {
+    return await call(model, method, [ids].concat(args));
+  }
+
+  async function workflowSignal(model: string, ids: any[], signal: string) {
+    const uid = await login();
+    return await commonClient.methodCall("exec_workflow", [
+      db,
+      uid,
+      password,
+      model,
+      signal,
+      ids[0],
+    ]);
+  }
+
+  async function searchRead(
+    model: string,
+    domain: any[],
+    fields: any[] = [],
+    kwargs: any = {},
+    context: any = {}
+  ) {
+    const data = await call(model, "search_read", [domain, fields], {
+      ...kwargs,
+      context,
+    });
+    return data;
+  }
+
+  async function filterGetResults(filterId: string, fields: string[], kwargs: any = {}, context: any = {}) {
+    const response_filter = await get('ir.filters', [parseInt(filterId)]) as any;
+    const response_filter_data = response_filter[0];
+    // return response_filter_data;
+    const response_filter_domain = parseDomain(response_filter_data.domain);
+    const data = await searchRead(response_filter_data.model_id, response_filter_domain, fields, kwargs, context)
+    return data;
+  }
+
+  return {
+    login,
+    call,
+    search,
+    read,
+    get,
+    write,
+    create,
+    unlink,
+    fieldsGet,
+    nameGet,
+    nameSearch,
+    nameCreate,
+    defaultGet,
+    onChange,
+    workflowSignal,
+    searchRead,
+    filterGetResults
+  };
+}
+
+export { createOdooClient };
